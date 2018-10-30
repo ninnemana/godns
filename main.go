006dbb53a41c4cdbf36840905ce6ddaf9a5cd50b
Alex Ninneman <alex@ninneman.org>
2018-10-29 23:27:55 -0500

    reads config file to start service

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/ninnemana/godns/service"
 )
 
+var (
+	configFile = flag.String("config", "", "declares the path of the config file")
+)
+
 func main() {
-	svc, err := service.New()
+	flag.Parse()
+	if configFile == nil || *configFile == "" {
+		log.Fatalf("invalid config file")
+	}
+
+	svc, err := service.New(*configFile)
 	if err != nil {
 		log.Fatalf("failed to create service: %v", err)
 	}
